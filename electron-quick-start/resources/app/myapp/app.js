66abe794f48476e873e76413ccad37bdc526a769
Baptiste Dumont <baptiste.dumont@epitech.eu>
2019-05-07 16:26:41 +0200

    path and file magnet

--- electron-quick-start/resources/app/myapp/app.js
+++ electron-quick-start/resources/app/myapp/app.js
@@ -3,12 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded());
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
